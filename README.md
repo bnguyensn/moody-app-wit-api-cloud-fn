0f92ee489258709324c1e20ceb653a01e28393b9
Binh Nguyen <b.nguyen.sn@gmail.com>
2020-03-03 21:06:54 +0000

    feat: first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Moody App GCP Cloud Function - wit.ai API
+
+This is the Google Cloud Platform wit.ai API cloud function for Moody App.
+
+The cloud function calls [wit.ai](https://wit.ai)'s sentiment endpoint whenever new records appear in Firestore.
---
+++ index.js
@@ -0,0 +1,9 @@
+// This is a ...
+//
+// https://cloud.google.com/functions/docs/env-var#accessing_environment_variables_at_runtime
+// https://cloud.google.com/functions/docs/env-var#nodejs_6_nodejs_8_python_37_and_go_111
+// https://cloud.google.com/functions/docs/monitoring/error-reporting
+
+const fetch = require('node-fetch');
+
+exports.moody = async (e, context) => {};
